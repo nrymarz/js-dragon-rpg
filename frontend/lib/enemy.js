43ba24d223a103f4c620ab117f5ecb0871f04d10
Nathan Rymarz <nrymarz@gmail.com>
2021-02-16 15:21:37 -0600

    changed enemy draw method to parent class and added class variables to each child class

--- frontend/lib/enemy.js
+++ frontend/lib/enemy.js
@@ -5,46 +5,54 @@ class Enemy{
         this.image = document.createElement('img')
         this.image.src = "./lib/images/DAGRONS5.png"
     }
+
+    draw(ctx){
+        ctx.drawImage(this.image,this.spritePixelIndex[0],this.spritePixelIndex[1],this.spritePixelWidth,this.spritePixelHeight,this.x,this.y,this.width,this.height)
+    }
 }
 
 
 class EasyEnemy extends Enemy{
     constructor(){
         super()
-    }
-
-    draw(ctx){
-        ctx.drawImage(this.image,0,160,100,100,this.x,this.y,100,100)
+        this.width = 100
+        this.height = 100
+        this.spritePixelIndex=[0,160]
+        this.spritePixelWidth = 100
+        this.spritePixelHeight = 100
     }
 }
 
 class MediumEnemy extends Enemy{
     constructor(){
         super()
-    }
-
-    draw(ctx){
-        ctx.drawImage(this.image,0,0,100,150,this.x,this.y,125,180)
+        this.width = 125
+        this.height = 180
+        this.spritePixelIndex=[0,0]
+        this.spritePixelWidth = 100
+        this.spritePixelHeight = 150
     }
 }
 
 class HardEnemy extends Enemy{
     constructor(){
         super()
-    }
-
-    draw(ctx){
-        ctx.drawImage(this.image,200,0,100,110,this.x,this.y,150,225)
+        this.width = 150
+        this.height = 225
+        this.spritePixelIndex=[200,0]
+        this.spritePixelWidth = 100
+        this.spritePixelHeight = 110
     }
 }
 
 class VeryHardEnemy extends Enemy{
     constructor(){
         super()
-    }
-
-    draw(ctx){
-        ctx.drawImage(this.image,100,280,200,150,this.x,this.y,400,300)
+        this.width = 400
+        this.height = 300
+        this.spritePixelIndex=[100,280]
+        this.spritePixelWidth = 200
+        this.spritePixelHeight = 150
     }
 }
 
